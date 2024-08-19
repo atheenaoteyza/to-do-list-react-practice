af47d39e9f369a77104c233c2abd304540c87a9a
atheenaoteyza <alyboteyza@iskolarngbayan.pup.edu.ph>
2024-08-19 17:15:21 +0800

    No commit message

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -63,9 +63,7 @@ const ToDoList = () => {
                   width="21"
                   fill="#FFFFFF"
                 >
-                                 {" "}
                   <path d="M261-120q-24 0-42-18t-18-42v-570h-41v-60h188v-30h264v30h188v60h-41v570q0 24-18 42t-42 18H261Zm106-146h60v-399h-60v399Zm166 0h60v-399h-60v399Z"></path>
-                               {" "}
                 </svg>
               </button>
             </li>
